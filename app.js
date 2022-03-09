dde54da0e7afd50be0e08b273c9763744de67095
subender <subender.io@outlook.com>
2022-03-10 02:06:13 +0530

    created cards array

--- app.js
+++ app.js
@@ -1,1 +1,40 @@
-console.log("test");
+const cards = [
+  {
+    name: "bear",
+    img: "img/bear.jpg",
+  },
+
+  {
+    name: "chicken",
+    img: "img/chicken.jpg",
+  },
+  {
+    name: "dog",
+    img: "img/dog.jpg",
+  },
+  {
+    name: "frog",
+    img: "img/frog.jpg",
+  },
+  {
+    name: "koala",
+    img: "img/koala.jpg",
+  },
+  {
+    name: "monkey",
+    img: "img/monkey.jpg",
+  },
+
+  {
+    name: "panda",
+    img: "img/panda.jpg",
+  },
+  {
+    name: "pig",
+    img: "img/pig.jpg",
+  },
+  {
+    name: "tiger",
+    img: "img/tiger.jpg",
+  },
+];
